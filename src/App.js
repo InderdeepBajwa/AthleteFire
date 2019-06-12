ac46ea231af7d67b944d8260f6841dbaa8536653
Inderdeep Singh Bajwa <InderdeepBajwa@users.noreply.github.com>
2019-06-11 20:49:55 -0400

    Authentication patch

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
 import React from 'react';
 //import './App.css';
-import {BrowserRouter as Router, Route, Link } from "react-router-dom";
+import {BrowserRouter as Router } from "react-router-dom";
 import NavigationBar from './partials/navbar';
 
-import Firebase, { FirebaseContext } from './firebase';
+import { withAuthentication } from './session';
 
 
-function App() {
-  return (
-    <FirebaseContext.Provider value={new Firebase()}>
-      <Router>
-        <NavigationBar />
-      </Router>
-    </FirebaseContext.Provider>
+const App = () => (
+    <Router>
+      <NavigationBar />
+    </Router>
   );
-}
 
-export default App;
+
+export default withAuthentication(App);
+// export default App;
---
+++ src/session/index.js
@@ -0,0 +1,12 @@
+
+import AuthUserContext from './context';
+import withAuthentication from './withAuthentication';
+import withAuthorization from './withAuthorization';
+
+// TODO: with Authorization and email verification (optional)
+
+export {
+    AuthUserContext,
+    withAuthentication,
+    withAuthorization
+};
